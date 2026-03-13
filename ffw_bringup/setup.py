5957cb8ec9ffc1574ab1cf82cff9f763aebe04ed
Hyunwoo Nam <nhw@robotis.com>
2026-03-13 09:26:20 +0900

    Added SH5
    Signed-off-by: Hyunwoo Nam <nhw@robotis.com>

--- ffw_bringup/setup.py
+++ ffw_bringup/setup.py
@@ -29,6 +29,8 @@ setup(
          glob('config/ffw_bg2_rev4_follower/*')),
         (os.path.join('share', package_name, 'config/ffw_sg2_rev1_follower'),
          glob('config/ffw_sg2_rev1_follower/*')),
+        (os.path.join('share', package_name, 'config/ffw_sh5_rev1_follower'),
+         glob('config/ffw_sh5_rev1_follower/*')),
         (os.path.join('share', package_name, 'config/ffw_lg2_leader'),
          glob('config/ffw_lg2_leader/*')),
         (os.path.join('share', package_name, 'config/ffw_lg2_rev2_leader'),
